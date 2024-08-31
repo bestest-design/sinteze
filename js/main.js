5e144022bd64f18f5cac271f49507e18d10689bc
Imants <imants.kokis@gmail.com>
2024-08-31 20:33:16 +0300

    feat: add overlay

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 // main.js
 
-function removeOverly() {
+document.addEventListener("DOMContentLoaded", function () {
   const overlay = document.querySelector("overlay");
-  playAudio();
-  overlay.style.transition = "opacity 0.5s ease-out";
-  overlay.style.opacity = "0";
-  setTimeout(() => {
-    overlay.remove();
-  }, 500);
-}
+
+  function removeOverly() {
+    playAudio();
+    // Start the fade-out effect
+    overlay.style.transition = "opacity 0.5s ease-out"; // Set the duration and easing of the fade-out
+    overlay.style.opacity = "0"; // Start the fade-out by setting opacity to 0
+
+    // After the transition ends, remove the overlay from the DOM
+    setTimeout(() => {
+      overlay.remove();
+    }, 500); // Timeout duration matches the transition duration
+  }
+
+  // Attach the removeOverly function to the click event
+  overlay.addEventListener("click", removeOverly);
+});
